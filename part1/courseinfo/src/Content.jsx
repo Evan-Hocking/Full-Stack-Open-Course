f448a707c06077662d5be47d33189f7be8a6f20e
Ev Hocking <95835168+Evan-Hocking@users.noreply.github.com>
2026-09-08 12:39:20 +0000

    incomplete

---
+++ part1/courseinfo/src/Content.jsx
@@ -0,0 +1,19 @@
+const Content = (props) => {
+
+    return (
+        <>
+            
+            <p>
+                {part1} {exercises1}
+            </p>
+            <p>
+                {part2} {exercises2}
+            </p>
+            <p>
+                {part3} {exercises3}
+            </p>
+        </>
+    )
+}
+
+export default Content
